efb3992b6f6fa3bca1b9843c9a7626809de6bd4e
IsaacCodes <baconboyxy@deter.com>
2024-09-10 19:15:50 +0000

    Edited problem 1 description and correct input/output

--- problem01/solution.py
+++ problem01/solution.py
@@ -1,6 +1,7 @@
 def solution(side_length: float) -> int:
     return int((side_length**3 * 19.254 / 1000) // 1)
 
-
+from input_output import io_dict
 if __name__ == "__main__":
-    print(solution(float(input())))
+    for inpt in io_dict:
+        print(inpt, solution(inpt), solution(inpt) == io_dict[inpt])
