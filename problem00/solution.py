3cedb723c2cdc7f60e6ae9b66fa09e7dfcbf1ff2
Trevor Douglas <145886230+TrevorD2@users.noreply.github.com>
2024-09-11 17:00:54 +0000

    Updates testcases and solutions

--- problem00/solution.py
+++ problem00/solution.py
@@ -1,5 +1,7 @@
 def solution(participant: str) -> str:
     return "Hello "+participant+", welcome to the RHS Programming Competition!"
 
+from testcases import io_dict
 if __name__=="__main__":
-    print(solution(input()))
+    for i in io_dict:
+        print(solution(i), solution(i)==io_dict[i])
--- problem02/solution.py
+++ problem02/solution.py
@@ -1,5 +1,7 @@
 def solution(u: list[int], v: list[int])->int:
     return sum([u[i]*v[i] for i in range(len(u))])
 
+from testcases import io_dict
 if __name__ == "__main__":
-    print(solution(list(map(int, input().split())),list(map(int, input().split()))))
+    for i in io_dict:
+        print(solution(i[0],i[1]))
