07f86f0655251613a8d5861bf80a8e25ffb2f80d
Trevor Douglas <145886230+TrevorD2@users.noreply.github.com>
2024-09-11 22:09:42 +0000

    finished problem04

--- problem04/solution.py
+++ problem04/solution.py
@@ -1,23 +1,26 @@
 def solution(D:int, C:int, ciphertext:str)->str:
+    alph = "0123456789P"
 
     multiplicative_inverse = -1
-    for i in range(0,10):
-        if (i*D)%10==1: 
+    for i in range(0,11):
+        if (i*D)%11==1: 
             multiplicative_inverse = i
             break
-    print(multiplicative_inverse)
 
-    additive_inverse = 10-(C%10)
+    additive_inverse = 11-(C%11)
 
     def mapping(integer:int)->int:
-        return (multiplicative_inverse*(additive_inverse+integer)%10) % 10
+        return (multiplicative_inverse*(additive_inverse+integer)) % 11
     
     result = []
 
     for i in ciphertext:
-        result.append(str(mapping(int(i))))
+        result.append(alph[mapping(alph.index(i))])
 
     return "".join(result)
 
+from testcases import io_dict
 if __name__=="__main__":
-    print(solution(3, 1, "32"))
+    for i in io_dict:
+        print(len(i[2]))
+        print(solution(i[0], i[1], i[2]), solution(i[0], i[1], i[2])==io_dict[i])
