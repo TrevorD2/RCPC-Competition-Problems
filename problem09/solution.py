e865ce0ad4be6cb731ee81c9b407aecff3dacd94
IsaacCodes <baconboyxy@deter.com>
2024-09-13 17:34:29 +0000

    Finsihed Problem 9

--- problem09/solution.py
+++ problem09/solution.py
@@ -14,12 +14,20 @@ def solution(measurement: float, unit: str, ingrediant: str) -> str:
     }
 
     if unit in weight_convert:
-        metric_measurement = measurement * vol_convert[unit] *  28.35
-    if unit in vol_convert:
+        metric_measurement = measurement * weight_convert[unit] *  28.35
+        metric_unit = "g"
+    elif unit in vol_convert:
         metric_measurement = measurement * vol_convert[unit] *  29.57
+        metric_unit = "mL"
+    else:
+        metric_measurement = measurement
+        metric_unit = unit
+
+
+    return f"{round(metric_measurement*10)/10} {metric_unit} of {ingrediant}"
 
 
 from testcases import io_dict
 if __name__ == "__main__":
     for inpt in io_dict:
-        print(f"{inpt}\n{solution(inpt)}\n{solution(inpt) == io_dict[inpt]}\n")
+        print(f"{inpt}\n{solution(*inpt)}\n{solution(*inpt) == io_dict[inpt]}\n")
