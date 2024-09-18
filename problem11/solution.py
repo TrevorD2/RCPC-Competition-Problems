eadf63ca2970f429cb2aefa45ef5d4fcd69963b0
IsaacCodes <baconboyxy@deter.com>
2024-09-18 16:31:19 +0000

    Started Problem 11 + other minor changes

---
+++ problem11/solution.py
@@ -0,0 +1,11 @@
+def solution(funhouse: str, start_pos: list[int]) -> list[int]:
+    tophat_pos = (0, 0)
+
+
+    return tophat_pos
+
+
+from testcases import io_dict
+if __name__ == "__main__":
+    for inpt in io_dict:
+        print(f"{inpt}\n{solution(*inpt)}\n{solution(*inpt) == io_dict[inpt]}\n")
