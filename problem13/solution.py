ad29e7f4a0169385d1fb751e76f145dd4d0b2279
IsaacCodes <baconboyxy@deter.com>
2024-09-21 20:43:14 -0700

    Finished problem 13 (/w 1 testcase)

--- problem13/solution.py
+++ problem13/solution.py
@@ -1,9 +1,42 @@
-def solution(landscape: str, start_pos: list[int], start_direction: str) -> list[int]:
-    pass
+def solution(landscape_str: str) -> str:
+    col_nums = [int(x) for x in landscape_str.split("\n")[0]]
+    landscape = [list(row) for row in landscape_str.split("\n")[1:]]
+
+    def next_gen(landscape):
+        for y, row in enumerate(landscape):
+            for x, char in enumerate(row):
+                if char == "*":
+                    if landscape[y+1][x] == " ":
+                        landscape[y][x] = " "
+                        landscape[y+1][x] = "*"
+
+                    elif landscape[y+1][x] == "/" and landscape[y][x-1] == " ":
+                        landscape[y][x] = " "
+                        landscape[y][x-1] = "*"
+
+                    elif landscape[y+1][x] == "\\" and landscape[y][x+1] == " ":
+                        landscape[y][x] = " "
+                        landscape[y][x+1] = "*"
+
+        for x, n in enumerate(col_nums):
+            if n > 0:
+                col_nums[x] -= 1
+                landscape[0][x] = "*"
+
+    last_landscape = ""
+    while landscape != last_landscape:
+        last_landscape = [row.copy() for row in landscape]
+        next_gen(landscape)
+    
+
+    return "\n".join(["".join(row) for row in landscape])
 
 
 from testcases import io_dict
 if __name__ == "__main__":
     for inpt in io_dict:
         sol = solution(inpt)
-        print(f"{inpt}\n{sol}\n{sol == io_dict[inpt]}\n")
+        print(f"{inpt}\n\n{sol}\n{sol == io_dict[inpt]}\n")
+
+        with open("problem13/out.txt", "w") as f:
+            f.write(repr(sol))
--- problem13/testcases.py
+++ problem13/testcases.py
@@ -1,9 +1,24 @@
 io_dict = {
-    
+    '1032210100203101\n                \n                \n                \n                \n       /#\\      \n      /   \\     \n     /     \\    \n    /|     |\\   \n     |     |    \n################' : '                \n                \n                \n                \n       /#\\      \n   ** /   \\**   \n  ***/     \\**  \n  **/|     |\\*  \n* ** |     | * *\n################'
 }
 
 if __name__ == "__main__":
-    t_case ="""
-hi
+    t_case =r"""
+1032210100203101
+
+
+
+
+       /#\
+      /   \
+     /     \
+    /|     |\
+     |     |
+################
 """
-    print(t_case[1:])
+
+
+    length = len(t_case.split("\n")[1])
+    t_case2 = "\n".join([row.ljust(length) for row in t_case[1:-1].split("\n")])
+    with open("problem13/out.txt", "w") as f:
+            f.write(repr(t_case2))
