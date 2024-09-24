103049ac6dfc85126a86611007825602712586fc
Trevor Douglas <145886230+TrevorD2@users.noreply.github.com>
2024-09-24 16:31:13 +0000

    finished problem20

--- problem20/solution.py
+++ problem20/solution.py
@@ -10,7 +10,7 @@ def solution(connections: list[list[int]], treasures: list[int], actions: str, s
     
     adj_list = convert_to_adj_list(connections)
 
-    def e_closure(state, stateset):
+    def e_closure(state, stateset=set()):
         stateset.add(state)
         if state not in adj_list: return stateset
         for next, transition in adj_list[state]:
@@ -18,50 +18,24 @@ def solution(connections: list[list[int]], treasures: list[int], actions: str, s
                 stateset.update(e_closure(next, stateset))
         return stateset
 
-    det_adj_list = {}
-    nd_to_d_map = {}
-    
-    stack = []
-    processed = set()
-    initial = e_closure(start, set())
+    cur_state = frozenset(e_closure(start))
+    for action in actions:
+        next_state = set()
 
-    nd_to_d_map[frozenset(initial)] = 0
-    d_num = 1
+        for substate in cur_state:
+            if substate not in adj_list: continue
 
-    stack.append(initial)
-    while stack:
-        state = stack.pop()
-        if frozenset(state) in processed: continue
-        processed.add(frozenset(state))
+            for next, transition in adj_list[substate]:
+                if transition==int(action):
+                    next_state.update(e_closure(next))
+        cur_state = frozenset(next_state)
 
-        for action in range(2):
-            newstate = set()
-            
-            for subnode in state:
-                if subnode not in adj_list: continue
-                for next, transition in adj_list[subnode]:
-                    if transition == action: newstate.add(frozenset(e_closure(next, set())))
+    for substate in cur_state:
+        if substate in treasures: return True
 
-            if frozenset(newstate) not in nd_to_d_map:
-                nd_to_d_map[frozenset(newstate)] = d_num
-                stack.append(newstate)
-                d_num+=1
-            
-            accepting = False
-            for possible in newstate:
-                if list(possible)[0] in treasures: accepting = True
-
-            if frozenset(state) not in det_adj_list:
-                det_adj_list[nd_to_d_map[frozenset(state)]] = [(nd_to_d_map[frozenset(newstate)], action, accepting)]
-
-            else:
-                det_adj_list[nd_to_d_map[frozenset(state)]].append((nd_to_d_map[frozenset(newstate)], action, accepting))
-
-    print(det_adj_list, adj_list)
-
-    return True
+    return False
 
 from testcases import io_dict
 if __name__ == "__main__":
     for i in io_dict:
-        print(solution(*i))
+        print(solution(*i), solution(*i)==io_dict[i])
