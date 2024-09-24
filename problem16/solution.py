5a0b0d16c28dde671119242843b49659315eaf9e
IsaacCodes <baconboyxy@deter.com>
2024-09-24 15:56:30 +0000

    sol

--- problem16/solution.py
+++ problem16/solution.py
@@ -21,6 +21,32 @@ def solution(prices:list[float], springs:list[int], min_force:int, x:float) -> f
     ans = rec(0, 0)
     return ans if ans!=float("INF") else -1
 
+def solution(prices:list[float], springs:list[int], min_force:int, x:float) -> float:
+    min_k = min_force/x
+    price_springs = [(prices[i], springs[i]) for i in range(len(prices))]
+    min_p = float("inf")
+
+    def recur(remaining, tp, tk):
+        nonlocal min_p
+
+        for ps in remaining:
+            if tk >= min_k:
+                if tp < min_p:
+                    min_p = tp
+                continue
+
+            n_remaining = remaining.copy()
+            n_remaining.remove(ps)
+            n_tp, n_tk = tp + ps[0], tk + ps[1]
+
+            recur(n_remaining, n_tp, n_tk)
+
+
+    recur(price_springs.copy(), 0, 0)
+    
+    return min_p if min_p != float("inf") else -1
+
+
 from testcases import io_dict
 if __name__ == "__main__":
     for i in io_dict:
