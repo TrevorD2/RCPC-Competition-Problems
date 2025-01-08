6ebc33a5917e121500ff6148ddd6d5ebd1c84b71
IsaacCodes <baconboyxy@deter.com>
2025-01-08 17:22:36 +0000

    Reformatted problem 7 input, added 1 more testcase to 11.

--- problem11/testcases.py
+++ problem11/testcases.py
@@ -1,15 +1,19 @@
 io_dict = {
-    ('               \n          /  \\ \n               \n               \n               \n    x     /    \n               \n               \n     /        \\\n               \n             \\/\n               \n               \n               \n               ', (4, 5), "right") : (5, 14)
+    ('               \n          /  \\ \n               \n               \n               \n          /    \n               \n               \n     /        \\\n               \n             \\/\n               \n               \n               \n               ', (4, 5), "right") : (5, 14),
+    ('         /         \\\n                    \n                    \n                    \n                    \n                    \n                    \n                    \n                    \n                    \n/                   \n         /          \n                    \n                    \n\\                  /', (9, 10), "left") : (0, 11)
 }
 
 if __name__ == "__main__":
-    v = [[" " for x in range(15)] for y in range(15)]
-    v[5][4] = "x"
-    v[5][10] = "/"
-    v[1][10] = "/"
-    v[1][13] = "\\"
-    v[10][13] = "\\"
-    v[10][14] = "/"
-    v[8][14] = "\\"
-    v[8][5] = "/"
+    v = [[" " for x in range(20)] for y in range(15)]
+    #v[10][9] = "x"
+    v[10][0] = "/"
+    v[14][0] = "\\"
+    v[14][19] = "/"
+    v[0][19] = "\\"
+    v[0][9] = "/"
+    v[11][9] = "/"
+
+
+    for i, line in enumerate(v):
+        print("".join(line) + "! " + str(i))
     print(repr("\n".join(["".join(val) for val in v])))
