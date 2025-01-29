ef833c988ac7a4d154231b8a8a82efd6a1bf338c
IsaacCodes <baconboyxy@deter.com>
2025-01-29 20:11:28 +0000

    Problem 13 should be all done now

--- problem13/solution.py
+++ problem13/solution.py
@@ -3,32 +3,41 @@ def solution(landscape_str: str) -> str:
     landscape = [list(row) for row in landscape_str.split("\n")[1:]]
 
     def next_gen(landscape):
+
+        delta = False
+
         for y, row in enumerate(landscape):
             for x, char in enumerate(row):
                 if char == "*":
                     if landscape[y+1][x] == " ":
                         landscape[y][x] = " "
                         landscape[y+1][x] = "*"
+                        delta = True
 
-                    elif landscape[y+1][x] == "/" and landscape[y][x-1] == " ":
+
+                    elif landscape[y+1][x] == "/" and landscape[y+1][x-1] == " ":
                         landscape[y][x] = " "
-                        landscape[y][x-1] = "*"
+                        landscape[y+1][x-1] = "*"
+                        delta = True
 
-                    elif landscape[y+1][x] == "\\" and landscape[y][x+1] == " ":
+                    elif landscape[y+1][x] == "\\" and landscape[y+1][x+1] == " ":
                         landscape[y][x] = " "
-                        landscape[y][x+1] = "*"
+                        landscape[y+1][x+1] = "*"
+                        delta = True
 
-        for x, n in enumerate(col_nums):
-            if n > 0:
-                col_nums[x] -= 1
-                landscape[0][x] = "*"
+        if not delta:
+            for x, n in enumerate(col_nums):
+                if n > 0:
+                    col_nums[x] -= 1
+                    landscape[0][x] = "*"
+                    delta = True
 
-    last_landscape = ""
-    while landscape != last_landscape:
-        last_landscape = [row.copy() for row in landscape]
-        next_gen(landscape)
+        return delta
+
+    ret = True
+    while ret:
+        ret = next_gen(landscape)
     
-
     return "\n".join(["".join(row) for row in landscape])
 
 
@@ -37,6 +46,3 @@ if __name__ == "__main__":
     for inpt in io_dict:
         sol = solution(inpt)
         print(f"{inpt}\n\n{sol}\n{sol == io_dict[inpt]}\n")
-
-        with open("problem13/out.txt", "w") as f:
-            f.write(repr(sol))
