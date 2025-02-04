4e8306cc8ec022130386cc0aeab9ffb659d8f3f9
IsaacCodes <baconboyxy@deter.com>
2025-02-04 19:18:11 +0000

    Modded 7 to fit DSL standards

--- problem07/solution.py
+++ problem07/solution.py
@@ -1,10 +1,9 @@
-def solution(coaster: str) -> float:
-    slices = coaster.split("\n")
-    y = float(slices[0].split("|")[0])
-    dy = float(slices[0].split("|")[0]) - float(slices[1].split("|")[0])
+def solution(coaster: list[str]) -> float:
+    y = float(coaster[0].split("|")[0])
+    dy = float(coaster[0].split("|")[0]) - float(coaster[1].split("|")[0])
 
     beam_length = 0
-    for slice in slices[:-1]:
+    for slice in coaster[:-1]:
         y -= dy
         beam_length += slice.count("#")*y
 
@@ -14,4 +13,7 @@ def solution(coaster: str) -> float:
 from testcases import io_dict
 if __name__ == "__main__":
     for inpt in io_dict:
+
+        inpt = inpt.split("\n")
+
         print(f"{inpt}\n{solution(inpt)}\n{solution(inpt) == io_dict[inpt]}\n")
